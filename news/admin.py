317371bd7af42cb44edfb32775dccd237941a723
Grigoash Pasha <GrigorashPasha@gmail.com>
2021-10-10 15:26:45 +0300

    first app

---
+++ news/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from .models import News, Category
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'created_date', 'updated_date', 'publish')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content',)
+    list_editable = ('publish',)
+    list_filter = ('publish', 'category',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
